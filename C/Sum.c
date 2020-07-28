0cbc8958dde3dd40b3dd6eaaaacd197b0a202561
yong-jhih <waityouforever77@gmail.com>
2020-07-28 23:20:21 +0800

    renew

---
+++ C/Sum.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+    int main(){
+        int integer1,integer2,sum;
+        printf("first Num:");
+        scanf("%d",&integer1);
+        printf("second Num:");
+        scanf("%d",&integer2);
+        sum = integer1 + integer2 ;
+        printf("sum = %d\n",sum);
+    }
