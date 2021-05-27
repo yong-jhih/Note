1186e6c8a503701fcc6b06d0c4796c61ac9630c2
Scorpius <waityouforever77@gmail.com>
2021-05-27 17:35:21 +0800

    add node

--- test/Rate/stock.js
+++ test/Rate/stock.js
@@ -1,15 +1,20 @@
 const puppeteer = require('puppeteer');
-
-(async () => {
-    const browser = await puppeteer.launch({
-        executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
-        headless: false
-    });
-    const page = await browser.newPage();
-    await page.setViewport({
-        width: 1920,
-        height: 1080
-    });
-    await page.goto('https://www.skis.com.tw/n/Report');
-    await browser.close();
-})();
+url = ['https://www.skis.com.tw/nsk/research/1020944/1020944_0.HTM',
+    'https://www.skis.com.tw/nsk/research/1020945/1020945_0.HTM',
+    'https://www.skis.com.tw/nsk/research/1020946/1020946_0.HTM'
+];
+url.forEach(element => {
+    (async () => {
+        const browser = await puppeteer.launch({
+            executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
+            headless: false
+        });
+        const page = await browser.newPage();
+        await page.setViewport({
+            width: 1920,
+            height: 1080
+        });
+        await page.goto(element);
+        // await browser.close();
+    })();
+});
